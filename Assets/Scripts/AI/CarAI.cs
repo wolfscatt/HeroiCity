6b31ac4500640a59366f9d4fe4a5f9c3410c82be
wolfscatt <bjkeses11@hotmail.com>
2024-03-16 12:07:09 +0300

    WayPoints Created

---
+++ Assets/Scripts/AI/CarAI.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class CarAI : MonoBehaviour
+{
+    public GameObject wayPoint;
+    private Transform[] wayPoints;
+    private int currentWayPointIndex = 0;
+    private NavMeshAgent agent;
+
+    private void Start() {
+        wayPoints = wayPoint.GetComponentsInChildren<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        SetNextWaypoint();
+    }
+
+    void SetNextWaypoint()
+    {
+        if (currentWayPointIndex < wayPoints.Length)
+        {
+            agent.SetDestination(wayPoints[currentWayPointIndex].transform.position);
+        }
+        else
+        {
+            Debug.Log("Finish!");
+            // Yolun sonuna ulaşıldığında yapılacak işlemler buraya eklenebilir.
+        }
+    }
+
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            currentWayPointIndex++;
+            SetNextWaypoint();
+        }
+    }
+}
